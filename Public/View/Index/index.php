9acf6128f1948f06d8413636f9fd2436a2ab07f1
Yan Fonseca <yangoular@gmail.com>
2024-08-10 15:41:39 -0300

    alteração de variáveis

--- Public/View/Index/index.php
+++ Public/View/Index/index.php
@@ -45,27 +45,9 @@ if($_SESSION['logged'] != null){
                             echo $_SESSION['response'];
                         }
                     ?>
-                    <div class="d-grid"><button class="" type="submit">Entrar</button></div> <a href="" class="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Registre-se</a>
+                    <div class="d-grid"><button class="" type="submit">Login</button></div> <a href="../Index/registro.php">Register</a>
         
                 </form>
-            </div>
-                <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <?php include_once 'registro.php';?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
-                    </div>
-                    </div>
-                </div>
             </div>
         </body>
     </html>
