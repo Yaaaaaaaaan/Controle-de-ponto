7fdd2a8ddb72bbeff24d9e98bdecce0955cfdbe8
Yan <yangoular@gmail.com>
2025-04-14 01:32:38 -0300

    alteração de informações em menu.php e em index.php facilitação para visualizar impressão de variáveis em tela para depuração.

--- Public/View/User/index.php
+++ Public/View/User/index.php
@@ -50,9 +50,9 @@ foreach ($pointControlData as $row) {
         }
     }
 }
-echo $_SESSION['profileImagePath'];
-echo $_SESSION['userData'];
-echo $_SESSION['lastProfilePictures'];
+echo "profileImagePath: (à fazer) <br>".$_SESSION['profileImagePath'];
+echo "UserData: <br>".$_SESSION['userData'];
+echo "LastProfiles: <br>".$_SESSION['lastProfilePictures'];
 
 echo "<script>//console.log('Formato de daysData:', " . json_encode($daysData) . ");</script>";
 
