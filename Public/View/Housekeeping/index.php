63a9a05d11036fc84dafd4b6088a1faf226d337e
Yan <yangoular@gmail.com>
2025-04-24 02:26:29 -0300

    Tentativa de implementação do dashboard ao Housekeeping.

--- Public/View/Housekeeping/index.php
+++ Public/View/Housekeeping/index.php
@@ -1,6 +1,7 @@
-<?php
-    require '../layout/menu_housekeep.php';
-         ?>
+    <?php
+        require '../layout/menu_housekeep.php';
+
+    ?>
 
 <html>
     <head>
@@ -9,18 +10,55 @@
     <body>
     <div class="container-fluid">
         <div class="row mt-5">
-            <div class="col-md-12 mt-3">
-                Olá mundo!
+            <div class="col-md-4 mt-3">
+                <h3 class="mt-5">Estatísticas</h3>
+                <p class="span">Total de presenças no sistema</p>
+                <canvas id="myChart"></canvas>
+
+                <!-- TODO: fazer gráfico de pizza -->
+            </div>
+
+            <div class="col-md-4">
+
+
             </div>
         </div>
     </div>
 
-
-
-
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
         <script>
+
+            const data = {
+                labels: [
+                    'Recusado',
+                    'Já verificado',
+                    'Verificação pendente'
+                ],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        'rgb(173,181,189)',
+                        'rgb(32,201,151)',
+                        'rgb(253,126,20)'
+                    ],
+                    hoverOffset: 4
+                }]
+            };
+
+            const config = {
+                type: 'pie',
+                data: data,
+            };
+
+
+
         document.addEventListener('DOMContentLoaded', function() {
+            const ctx = document.getElementById('myChart').getContext('2d');
+            new Chart(ctx, config);
+
+
             const responseTheme = document.getElementById('responseTheme');
             const themeValue = responseTheme ? parseInt(responseTheme.textContent, 10) : 0;
             document.body.dataset.bsTheme = themeValue === 1 ? 'dark' : 'light';
