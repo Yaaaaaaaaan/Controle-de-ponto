0f6d77d8f8ecd185d200c9924cc121ad1cca5fa8
Yan <yangoular@gmail.com>
2025-05-20 19:01:20 -0400

    Refatoração

--- Public/service-worker.js
+++ Public/service-worker.js
@@ -1,79 +1,92 @@
-// ========================
-// 📁 service-worker.js
-// ========================
-const CACHE_NAME = 'controle-de-ponto-v1';
+const CACHE_NAME = 'v1'; // Nome do cache (pode ser versionado)
 const urlsToCache = [
-    '/',
-    '/controle-de-ponto/index.php', // Caminho correto para index.php
-    '/controle-de-ponto/View/Layout/menu.php', // Caminho correto para menu.php
-    'controle-de-ponto/Public/View/User/settings.php',
-    'controle-de-ponto/Public/View/User/community.php',
-    '/controle-de-ponto/Public/CSS/USR/handworking.css',
-    '/controle-de-ponto/Public/CSS/USR/dashboard.css',
-    '/controle-de-ponto/Public/JS/USR/userInterface.js',
-    '/controle-de-ponto/Public/JS/script.js',
+    //IndexedDB;
     '/controle-de-ponto/Public/JS/indexedDB/Config.js',
-    '/controle-de-ponto/Public/JS/indexedDB/Model.js'
-];
+    '/controle-de-ponto/Public/JS/indexedDB/Model.js',
+    '/controle-de-ponto/Public/JS/indexedDB/Controller.js',
 
-// Instalação do Service Worker
-self.addEventListener('install', function (event) {
+    //Index/index;
+    '/controle-de-ponto/Public/View/Index/index.php',
+    '/controle-de-ponto/Public/CSS/IDX/style.css',
+    '/controle-de-ponto/Public/JS/IDX/authController.js',
+
+    //Layout; (não necessário, pois ambos inicializam o cache)
+    //'/controle-de-ponto/Public/View/Layout/menu.php',
+    //'/controle-de-ponto/Public/View/Layout/menu_housekeep.php',
+
+    //User/index;
+    '/controle-de-ponto/Public/View/User/index.php',
+    '/controle-de-ponto/Public/CSS/USR/dashboard.css',
+    '/controle-de-ponto/Public/JS/USR/indexDashboard.js',
+
+    //User/settings;
+    '/controle-de-ponto/Public/View/User/settings.php',
+    '/controle-de-ponto/Public/CSS/USR/handworking.css',
+    '/controle-de-ponto/Public/JS/USR/userController.js',
+
+    //User/community;
+    '/controle-de-ponto/Public/View/User/community.php',
+
+    //Housekeeping/index;
+    '/controle-de-ponto/Public/View/Housekeeping/index.php',
+    '/controle-de-ponto/Public/JS/HKG/indexDashboard.js',
+
+    //Housekeeping/users;
+    '/controle-de-ponto/Public/View/Housekeeping/users.php',
+
+    //Demais arquivos;
+    '/controle-de-ponto/Public/JS/script.js'
+
+];
+self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Service Worker: Cache aberto e recursos adicionados');
+            .then(function(cache) {
+                console.log('Cache aberto!');
                 return cache.addAll(urlsToCache);
             })
-            .catch(error => console.error('Service Worker: Falha ao abrir o cache ou adicionar recursos:', error))
     );
 });
 
-// Ativação do Service Worker
-self.addEventListener('activate', function (event) {
-    const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', function(event) {
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.keys().then(function(cacheNames) {
+            return cacheNames.map(function(cacheName) {
+                if (cacheName !== CACHE_NAME) {
+                    return caches.delete(cacheName);
+                }
+            });
         })
-            .then(() => self.clients.claim()) // Força o Service Worker a controlar todas as páginas abertas
     );
-    console.log('Service Worker: Ativado');
 });
 
-// Intercepta requisições de fetch
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    // Retorna a resposta do cache se estiver disponível
-                    return response;
-                }
-                // Se não estiver no cache, busca da rede
-                return fetch(event.request).then(
-                    function (fetchResponse) {
-                        // Verifica se recebemos uma resposta válida
+self.addEventListener('fetch', function(event) {
+    const url = new URL(event.request.url);
+    // Intercepta a requisição para a página inicial e serve do cache
+    if (url.pathname === '/controle-de-ponto/index.php' && event.request.mode === 'navigate') {
+        event.respondWith(caches.match('/controle-de-ponto/Public/View/Index/Index.php'));
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request).then(function(fetchResponse) {
                         if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
                             return fetchResponse;
                         }
 
-                        // Se a resposta for válida, coloca no cache
                         const responseToCache = fetchResponse.clone();
 
                         caches.open(CACHE_NAME)
-                            .then(function (cache) {
+                            .then(function(cache) {
                                 cache.put(event.request, responseToCache);
                             });
 
                         return fetchResponse;
-                    }
-                );
-            })
-    );
+                    });
+                })
+        );
+    }
 });
